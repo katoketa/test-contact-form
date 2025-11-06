1190e7bedb332945e315301d8af4a1fc94a12a42
keita kato <infotec62509@gmail.com>
2025-11-06 17:01:03 +0900

    ダミーレコードの作成(エラー解決前)

---
+++ src/database/seeders/ContactsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ContactsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'category_id' => 2,
+            'first_name' => '山田',
+            'last_name' => '太郎',
+            'gender' => 1,
+            'email' => 'test@example.com',
+            'tel' => '08012345678',
+            'address' => '東京都渋谷区千駄ヶ谷1-2-3',
+            'building' => '千駄ヶ谷マンション101',
+            'detail' => '届いた商品が注文した商品ではありませんでした。\r\n商品の取り替えをお願いします。'
+        ];
+        for ($i = 1; i < 20; $i++) {
+            DB::table('contacts')->create($param);
+        }
+    }
+}
